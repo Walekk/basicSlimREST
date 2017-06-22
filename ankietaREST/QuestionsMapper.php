d29ef9ec2303d0337a7b28019e39cb09de7cb371
Jakub Walkowski <jakub.walkowski@rsqtechnologies.com>
2017-06-22 23:53:06 +0200

    Missing files

--- ankietaREST/QuestionsMapper.php
+++ ankietaREST/QuestionsMapper.php
@@ -1,4 +1,14 @@
 <?php
 class questionsMapper extends Mapper {
-
+    public function getTickets() {
+        $sql = "SELECT t.id, t.description, t.correctAnswear
+            from tickets t
+            join components c on (c.id = t.component_id)";
+        $stmt = $this->db->query($sql);
+        $results = [];
+        while($row = $stmt->fetch()) {
+            $results[] = new TicketEntity($row);
+        }
+        return $results;
+    }
 }
